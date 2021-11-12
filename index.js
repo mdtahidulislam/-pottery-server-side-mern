bc4408f2fd5bf90721113d89b976f026690d35a2
tahidul1 <contact.tahidul@gmail.com>
2021-11-12 10:48:19 +0600

    successfully conect db

--- index.js
+++ index.js
@@ -2,10 +2,29 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const port = process.env.PORT || 5000
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
+
+// db credential & client
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.apq2y.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri);
 
 // middlewire
 app.use(cors())
 app.use(express.json())
+
+// connect DBs
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db("Pottery");
+        const potteriesCollection = database.collection("potteries");
+
+    } finally {
+        //   await client.close();
+    }
+}
+run().catch(console.dir);
 
 // root api
 app.get('/', (req, res) => {
