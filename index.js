d5d61121d960a8593c8b00bbe1ec74875e5d00f6
tahidul1 <contact.tahidul@gmail.com>
2021-11-13 17:14:57 +0600

    created post order api successfully

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const port = process.env.PORT || 5000
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
 const { MongoClient } = require('mongodb');
 require('dotenv').config();
+const ObjectId = require('mongodb').ObjectId;
 
 // db credential & client
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.apq2y.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
@@ -19,6 +20,38 @@ async function run() {
         await client.connect();
         const database = client.db("Pottery");
         const potteriesCollection = database.collection("potteries");
+        const ordersCollection = database.collection("orders");
+
+        // GET API 6 POTTERIES
+        app.get('/potteries', async (req, res) => {
+            const query = {};
+            const cursor = potteriesCollection.find(query);
+            const potteries = await cursor.limit(6).toArray();
+            res.json(potteries)
+        })
+
+        // GET API ALL POTTERIES
+        app.get('/allpotteries', async (req, res) => {
+            const query = {};
+            const cursor = potteriesCollection.find(query);
+            const potteries = await cursor.toArray();
+            res.json(potteries)
+        })
+
+        // GET API SPECIFIC POTTERY
+        app.get('/potteries/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const pottery = await potteriesCollection.findOne(query);
+            res.send(pottery);
+        })
+
+        // POST ORDER
+        app.post('/orders', async (req, res) => {
+            const newOrder = req.body;
+            const result = await ordersCollection.insertOne(newOrder);
+            res.send(result);
+        })
 
     } finally {
         //   await client.close();
